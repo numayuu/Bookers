f854dc77e305e710888675f2f6034ab3cb24db6e
numayuu <n.you.01222@gmail.com>
2021-09-11 09:51:16 +0000

    add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,20 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(blog_params)
+    book.save
+    redirect_to books_path
   end
 
   def edit
+  end
+
+   private
+  def book_params
+    params.require(:book).permit(:title, :category, :body)
   end
 end
