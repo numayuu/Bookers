24a998522da06961bcaf8351adca5644809d570e
numayuu <n.you.01222@gmail.com>
2021-09-13 04:19:55 +0000

    start

--- db/schema.rb
+++ db/schema.rb
@@ -12,17 +12,8 @@
 
 ActiveRecord::Schema.define(version: 2021_09_10_042914) do
 
-  create_table "blogs", force: :cascade do |t|
-    t.string "title"
-    t.string "category"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "category"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
